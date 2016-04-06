c88783b364877a1323edc0e3745b9792dcb5e327
lbodor <lazar.bodor@gmail.com>
2016-04-06 19:39:01 +1000

    Fast forward only the checked out branches

--- fast-forward.sh
+++ fast-forward.sh
@@ -9,13 +9,19 @@ declare -a projects=(
     "geodesy-domain-rest"
 );
 
-cmd="git pull --ff-only && git submodule update --init --recursive --remote"
+# fetch and fast-forward the checkouted out branch and submodules
+function fast-forward {
+    echo "Fast-forward: `pwd`"
+    git fetch $(git rev-parse --symbolic-full-name --abbrev-ref @{upstream} | sed 's!/! !')
+    git merge --ff-only FETCH_HEAD
+    # TODO: Submodule update brings down all branches, we want just the tracked branch.
+    # Perhaps a separate script to do the initial cloning is the way to go.
+    git submodule update --init --recursive --remote
+}
 
-echo ${cmd}
-eval "${cmd}"
+fast-forward
 
 for p in ${projects[@]}
 do
-    echo $p: ${cmd}
-    (cd ../${p}; eval ${cmd})
+    (cd ../${p}; fast-forward)
 done

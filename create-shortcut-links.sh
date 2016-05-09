e8737c29a1b1bdc5f250fb88aac50c4d9e06438e
lbodor <lazar.bodor@gmail.com>
2016-05-09 20:27:20 +1000

    Add script to create some shortcut links

---
+++ create-shortcut-links.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ..
+ln -snf geodesy-domain-model domain
+ln -snf geodesyml-java-bindings bindings
+ln -snf geodesy-web-services web
+ln -snf geodesy-web-services-aws aws
+ln -snf GeodesyML ml

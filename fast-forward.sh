0def670a0a92ec7da0fbfc413f7ca58427150fb8
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-11-19 12:32:22 +1100

    Fix bug in fast forward script

--- fast-forward.sh
+++ fast-forward.sh
@@ -8,7 +8,10 @@ set -e
 # fetch and fast-forward the checkouted out branch and submodules
 function fast-forward {
     echo "Fast-forward: $(pwd)"
-    git fetch "$(git rev-parse --symbolic-full-name --abbrev-ref @'{upstream}' | sed 's!/! !')"
+
+    #shellcheck disable=SC2046
+    git fetch $(git rev-parse --symbolic-full-name --abbrev-ref @'{upstream}' | sed 's!/! !')
+
     git merge --ff-only FETCH_HEAD
     # TODO: Submodule update brings down all branches, we want just the tracked branch.
     # Perhaps a separate script to do the initial cloning is the way to go.

3757454b90a9be82f400f7bb701dbc0c954540e0
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-11-21 08:39:26 +1100

    Add all projects' dependencies to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,22 +1,36 @@
 let
-  pkgs = import <nixpkgs> {};
-  geodesyDevEnv = with pkgs; buildEnv {
-    name = "geodesyDevEnv";
+  defaultPkgs = import <nixpkgs> {};
+  pinnedPkgs = import (defaultPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "354fd3728952c229fee4f2924737c601d7ab4725";
+    sha256 = "052hfh3w2086zaaskx0y0dz4y6y2bpvj0xqlcarpbwn2ccnpph0j";
+  }) {};
+
+in
+
+{ nixpkgs ? pinnedPkgs }:
+
+let
+  pkgs = if nixpkgs == null then defaultPkgs else pinnedPkgs;
+  devEnv = with pkgs; buildEnv {
+    name = "devEnv";
     paths = [
-      awsEnv
-      javaEnv
-      typeScriptEnv
+      awscli
+      doxygen
       graphviz
+      maven3
+      nodejs-6_x
+      openjdk8
+      travis
     ];
   };
 in
-  pkgs.runCommand "dummy" {
+  pkgs.runCommand "setupEnv" {
     buildInputs = [
-      geodesyDevEnv
+      devEnv
     ];
+    shellHook = ''
+      export PATH="$(pwd)/../gnss-site-manager/node_modules/.bin:$PATH"
+    '';
   } ""
-
-
-
-
-
